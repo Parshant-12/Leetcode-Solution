575a32d6816cbd0bbd37120901f3fce27ef15cdb
Parshant Kumar <167954691+Parshant-12@users.noreply.github.com>
2026-03-31 19:24:23 +0530

    Time: 9 ms (14.18%), Space: 97 MB (53.55%) - LeetHub

---
+++ 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestAlternatingSubarray(vector<int>& nums, int threshold) {
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==0 && nums[i]<=threshold){
+                int currlen = 1;
+                for(int j=i+1;j<nums.size();j++){
+                    if(nums[j]<=threshold && nums[j]%2!=nums[j-1]%2){
+                        currlen++;
+                    }
+                    else{
+                        break;
+                    }
+                }
+                ans = max(ans,currlen);
+            }
+        }
+        return ans;
+    }
+};
